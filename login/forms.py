f3bebf954b146bff8def75ecbdca491d9e8f217b
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-15 22:18:11 +0000

    ok

--- login/forms.py
+++ login/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from register.models import Usuario
 
-class CustomLoginForm(forms.Form):
-    email = forms.EmailField(label='E-mail')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+class CustomLoginForm(forms.ModelForm):
+    senha = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Usuario
+        fields = ['email', 'senha']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remova o campo 'nome' do formulário
+        self.fields.pop('nome', None)

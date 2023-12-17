f0831edfabb1ecc25e563ecc01f8a82c4b05a8bd
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 19:49:20 +0000

    a

--- login/forms.py
+++ login/forms.py
@@ -1,9 +1,6 @@
 from django import forms
-from register.models import Usuario
 
 class CustomLoginForm(forms.ModelForm):
     senha = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
-    class Meta:
-        model = Usuario
-        fields = ['email', 'senha']
+   

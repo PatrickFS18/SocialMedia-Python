ad8056680670d0f6fa4d983f25264bdd89ad1ea5
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 19:52:21 +0000

    ok

--- login/forms.py
+++ login/forms.py
@@ -3,4 +3,5 @@ from django import forms
 class CustomLoginForm(forms.Form):
     senha = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
-   
+    def __str__(self):
+        return self.name
--- register/forms.py
+++ register/forms.py
@@ -3,4 +3,5 @@ from django import forms
 class RegisterForm(forms.ModelForm):
     senha = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
-   
+    def __str__(self):
+        return self.name

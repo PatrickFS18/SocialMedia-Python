04c3cbfa8bd6da27adf949475e96f26f15cb0af1
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 20:28:11 +0000

    a

--- register/views.py
+++ register/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegisterForm
+from login.forms import CustomLoginForm
 
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
-        messages.error(request,'Entrou aqui no 1')
+        formLogin = CustomLoginForm(request.POST)
+
         if form.is_valid():
-            messages.error(request,'Entrou aqui no 2')
-            
             # Salve o usuário no banco de dados
             form.save()
             # Adicione uma mensagem de sucesso
             messages.success(request, 'Registro bem-sucedido. Faça login para continuar.')
             # Redirecione para a página de login ou outra página desejada
-            return redirect('login:login')
+            return render(request,'login.html',{'form':formLogin})
         else:
-            messages.error(request,'Entrou aqui no 3')
-
+            # Adicione uma mensagem de erro
             messages.error(request, 'Corrija os erros no formulário.')
     else:
         form = RegisterForm()
-        messages.error(request,'Entrou aqui no 4')
-        
-    messages.error(request,'Entrou aqui no 5')
+
     return render(request, 'register.html', {'form': form})

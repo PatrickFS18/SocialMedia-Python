753c43f123f4fab1a158d68d28106a745323c4d0
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 19:48:55 +0000

    hm

--- login/forms.py
+++ login/forms.py
@@ -3,7 +3,7 @@ from register.models import Usuario
 
 class CustomLoginForm(forms.ModelForm):
     senha = forms.CharField(widget=forms.PasswordInput)
-
+    email = forms.EmailField()
     class Meta:
         model = Usuario
         fields = ['email', 'senha']

849388fa42a1a9b943b8a6cb626edda7709f49ff
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 19:51:51 +0000

    ok

--- register/forms.py
+++ register/forms.py
@@ -1,9 +1,6 @@
 from django import forms
-from .models import Usuario
 
 class RegisterForm(forms.ModelForm):
     senha = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = Usuario
-        fields = ['nome', 'email', 'senha']
+    email = forms.EmailField()
+   

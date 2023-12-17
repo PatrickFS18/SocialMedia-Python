b03be6aa1f3dca13141bb39a79176495e2f2d113
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 20:12:49 +0000

    ok

--- register/admin.py
+++ register/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'nome', 'is_active', 'is_staff')  # Adicione campos válidos do seu modelo
+    list_display = ('email', 'is_active', 'is_staff')  # Adicione campos válidos do seu modelo
     list_filter = ('is_active', 'is_staff')  # Adicione campos válidos do seu modelo
     ordering = ('email',)  # Adicione campos válidos do seu modelo
     filter_horizontal = ()  # Adicione campos válidos do seu modelo

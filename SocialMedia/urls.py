adc636e936e6cbd11a1e5b4f6a9c5aba8f042e93
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2023-12-17 19:21:31 +0000

    test

--- SocialMedia/urls.py
+++ SocialMedia/urls.py
@@ -19,8 +19,7 @@ from django.urls import include, path
 from login.views import login_view
 
 urlpatterns = [
-    path("", login_view, name="home"),
+    path("login/", login_view, name="home"),
     path("admin/", admin.site.urls),
-    path("login/", include("login.urls", namespace="login")),
     path("register/", include("register.urls", namespace="register")),
 ]
--- login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view
-
-app_name = 'login'  
-
-urlpatterns = [
-    path('', login_view, name='login'),
-]
